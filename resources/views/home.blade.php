dc028509ab9316ec24502204ec9efb92c400b9ff
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-11-29 20:57:36 +0530

    Updated

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -83,9 +83,9 @@
     <section class="banner">
         <div style="text-align: center; padding-top: 100px;">
             <h1 style="color: rgb(6, 4, 60); font-size: 3em;">Welcome to the Resource Booking System</h1>
-            <p style="color: rgb(255, 255, 255); font-size: 1.2em; margin-top: 20px;">Please log in to approve hall booking and quarter reservation applications.</p>
+            <p style="color: rgb(71, 66, 85); font-size: 1.2em; margin-top: 20px;">Please log in to approve hall booking and quarter reservation applications.</p>
             <a href="/login" style="display: inline-block; margin-top: 30px; padding: 15px 30px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; font-size: 1.2em;">Login</a>
-            <a href="/book-hall" style="display: inline-block; margin-top: 30px; padding: 15px 30px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; font-size: 1.2em; margin-left: 10px;">Book Hall</a>
+            <a href="{{ route('halls.book') }}" style="display: inline-block; margin-top: 30px; padding: 15px 30px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; font-size: 1.2em; margin-left: 10px;">Book Hall</a>
             <a href="/book-quarter" style="display: inline-block; margin-top: 30px; padding: 15px 30px; background-color: #007bff; color: white; text-decoration: none; border-radius: 5px; font-size: 1.2em; margin-left: 10px;">Book Quarter</a>
 
         </div>
