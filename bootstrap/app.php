110454b052fe896748914777bba39db8ee11535b
Isuru Udakara <pereraisuruudakara@gmail.com>
2025-12-05 18:30:18 +0530

    Updated

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,6 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
     $middleware->alias([
         'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+        'requester' => \App\Http\Middleware\EnsureUserIsRequester::class,
     ]);
 })
     ->withExceptions(function (Exceptions $exceptions): void {
