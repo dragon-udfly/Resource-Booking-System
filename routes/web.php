0b4fe62446f59989a62799d5b4ace2722b650d5f
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-02-01 13:45:43 +0530

    Restore button route added

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/login', function () {
 Route::post('/login', [UserController::class, 'login'])->name('login.submit');
 
 Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin', function() {
+    Route::get('/admin', function () {
         return view('adminpanel');
     })->name('admin');
 
@@ -42,20 +42,20 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::patch('/halls/{hall}', [HallController::class, 'update'])->name('halls.update');
     Route::delete('/halls/{hall}', [HallController::class, 'destroy'])->name('halls.destroy');
 
-    Route::get('/modifyaccount', function(){
+    Route::get('/modifyaccount', function () {
         return view('modifyaccount');
     });
 
-    Route::get('/modifyhall', function(){
+    Route::get('/modifyhall', function () {
         return view('modifyhall');
     });
-    
-    Route::get('/systemsetting', function(){
+
+    Route::get('/systemsetting', function () {
         return view('systemsetting');
     })->name('systemsetting');
 
-    Route::get('/modifyquarter', function(){
-    return view('modifyquarter');
+    Route::get('/modifyquarter', function () {
+        return view('modifyquarter');
     });
 
     Route::get('/auditlog', [UserController::class, 'showAuditLog'])->name('auditlog');
@@ -73,7 +73,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/marking-scheme', [QuarterAllocationController::class, 'markingScheme'])->name('marking-scheme.edit');
     Route::put('/marking-scheme', [QuarterAllocationController::class, 'updateMarkingScheme'])->name('marking-scheme.update');
 
-    Route::get('/addquarter', function(){
+    Route::get('/addquarter', function () {
         return view('addquarter');
     })->name('addquarter');
 
@@ -81,7 +81,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
 });
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/preference', function(){
+    Route::get('/preference', function () {
         return view('preference');
     })->name('preference');
     Route::post('/password/change', [UserController::class, 'changePassword'])->name('password.change');
@@ -109,6 +109,7 @@ Route::middleware(['auth'])->group(function () {
     Route::patch('/family-quarter-application/{id}/review', [QuarterAllocationController::class, 'updateFamilyQuarterReview'])->name('family-quarter.review.update');
     Route::get('/scheduled-quarter-application/{id}/review', [QuarterAllocationController::class, 'showScheduledQuarterReview'])->name('scheduled-quarter.review');
     Route::post('/scheduled-quarter-application/{id}/allocate', [QuarterAllocationController::class, 'allocateQuarter'])->name('scheduled-quarter.allocate');
+    Route::post('/scheduled-quarter-application/{id}/restore', [QuarterAllocationController::class, 'restoreScheduledQuarterApplication'])->name('scheduled-quarter.restore');
     Route::get('/quarter-application/{id}/download-pdf', [QuarterAllocationController::class, 'downloadPdf'])->name('quarter.download-pdf');
     Route::get('/processed-application/scheduled/{id}', [QuarterAllocationController::class, 'showProcessedScheduled'])->name('history.view_scheduled');
     Route::get('/processed-application/family/{id}', [QuarterAllocationController::class, 'showProcessedFamily'])->name('history.view_family');
