bade90661db29e46b3bc5c3bf72c83aefc5e2e4f
Isuru Udakara <pereraisuruudakara@gmail.com>
2026-02-07 22:46:58 +0530

    -testing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,30 +6,38 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-    $middleware->web([
-        \App\Http\Middleware\CheckDatabaseConnection::class,
-    ]);
+        $middleware->web([
+            \App\Http\Middleware\CheckDatabaseConnection::class,
+        ]);
 
-    $middleware->priority([
-        \App\Http\Middleware\CheckDatabaseConnection::class,
-        \Illuminate\Session\Middleware\StartSession::class,
-        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        \Illuminate\Auth\Middleware\Authenticate::class,
-        \Illuminate\Session\Middleware\AuthenticateSession::class,
-        \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        \Illuminate\Auth\Middleware\Authorize::class,
-    ]);
+        /* 
+        $middleware->validateCsrfTokens(except: [
+            '/login',
+            '/bookhall',
+            '/logout',
+        ]);
+        */
 
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
-        'requester' => \App\Http\Middleware\EnsureUserIsRequester::class,
-    ]);
-})
+        $middleware->priority([
+            \App\Http\Middleware\CheckDatabaseConnection::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Auth\Middleware\Authenticate::class,
+            \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \Illuminate\Auth\Middleware\Authorize::class,
+        ]);
+
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\EnsureUserIsAdmin::class,
+            'requester' => \App\Http\Middleware\EnsureUserIsRequester::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
